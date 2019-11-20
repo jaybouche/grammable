9c965bea77a5b0a17826099461a58a78094cec2f
jaybouche <jaybouche@gmail.com>
2019-11-20 00:35:51 +0000

    8

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,15 +4,24 @@ RSpec.describe GramsController, type: :controller do
   describe "grams#index action" do
     it "should successfully show the page" do
     get :index
-    except(response).to have_http_status(:success)
+    expect(response).to have_http_status(:success)
     end
-    end
+  end
 
   describe "grams#new action" do 
    it "should successfully show me the new form" do
     get :new
     expect(response).to have_http_status(:success)
    end
+  end
+  
+  describe "grams#create action" do
+   it "should successfully create a new gram in our database" do
+   post :create, params: { gram: {message: 'Hello!'} }
+   expect(response).to redirect_to root_path
 
+   gram = Gram.last
+   expect(gram.message).to eq("Hello!")
    end
+  end
 end
